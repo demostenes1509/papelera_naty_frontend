b6ffe082a3f844224e691d81e4f5163b5e64f991
Maximiliano Carrizo <maximiliano.carrizo@whiteprompt.com>
2018-12-19 15:39:52 -0300

    Navigation bar !

--- src/components/navigationbar/NavigationBar.js
+++ src/components/navigationbar/NavigationBar.js
@@ -4,7 +4,27 @@ class NavigationBar extends Component {
   render() {
     return (
       <section className="navigation-bar">
-        <p>NAVIGATION BAR 2</p>
+
+        <ul className="main-nav">
+          <li><a href="#" className="sub-menu-item">Inicio</a>
+            <ul className="sub-menu-list">
+              <li><a href="#">List item 1</a></li>
+              <li><a href="#">List item 2</a></li>
+              <li><a href="#">List item 3</a></li>
+              <li><a href="#">List item 4</a></li>
+              <li><a href="#">List item 5</a></li>
+              <li><a href="#">List item 6</a></li>
+              <li><a href="#">List item 7</a></li>
+              <li><a href="#">List item 8</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Mi Cuenta</a></li>
+          <li><a href="#">Contacto</a></li>
+          <li><a href="#">Descuentos</a></li>
+          <li><a href="#">Carrito</a></li>
+          <li className="main-search"><input type="text" className="search-input"/><button className="search-btn">Search</button></li>
+        </ul>
+
       </section>
     );
   }
