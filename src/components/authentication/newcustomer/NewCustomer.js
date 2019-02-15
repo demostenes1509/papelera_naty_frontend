41dabb840bbcde6a9a6560da22e28e9982908951
Maximiliano Carrizo <maximiliano.carrizo@whiteprompt.com>
2019-02-15 10:52:19 -0300

    Twitter

--- src/components/authentication/newcustomer/NewCustomer.js
+++ src/components/authentication/newcustomer/NewCustomer.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom';
-// import io from 'socket.io-client';
-// import {API_URL} from 'config';
 import OAuth from './OAuth';
-
-// const socket = io(API_URL);
 import { socket } from 'components/util/SocketUtil';
 
-const providers = ['google', 'facebook'];
-
+const providers = ['google', 'facebook','twitter'];
 
 class NewCustomer extends Component {
 
   render() {
-
     return (
       <div className="authentication-forms-container">
-				{providers.map(provider => 
-					<OAuth provider={provider} key={provider} socket={socket} />
-				)}
+				{providers.map(provider => <OAuth provider={provider} key={provider} socket={socket} />)}
       </div>
     );
 	}
